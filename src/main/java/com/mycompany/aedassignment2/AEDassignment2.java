72e77534cb2f5fe2534d21af6027600131b54737
Sanhita54742 <sanhita.san@northeastern.edu>
2022-10-27 12:51:33 -0400

    doctor panel

--- src/main/java/com/mycompany/aedassignment2/AEDassignment2.java
+++ src/main/java/com/mycompany/aedassignment2/AEDassignment2.java
@@ -1,17 +1,16 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.aedassignment2;
-
+import com.mycompany.aedassignment2.MainPage;
 /**
  *
  * @author Admin
  */
 public class AEDassignment2 {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+ public static void main(String[] args) {
+        MainPage obj = new MainPage();
+        obj.setVisible(true);
+    }   
 }
